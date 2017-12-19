c9ffb912ccb59750cea4b33e5d96278c57545bc2
MrNivorous <karn.sood@gmail.com>
2017-12-18 20:55:37 -0600

    Add Admin interface

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def access_denied(exception)
+    redirect to root_path, alert: exception.message
+  end
 end
